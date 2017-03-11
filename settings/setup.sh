53ee780c8b6ff7b5b4a38af6d6e9413d1c12dc23
piglovesyou <thepiglovesyou@gmail.com>
2017-03-11 17:17:21 +0900

    a

--- settings/setup.sh
+++ settings/setup.sh
@@ -19,10 +19,13 @@ sed -i.bak '/%wheel ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 # yaourt
-su - pig
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
-tar zxvf package-query.tar.gz
-(cd package-query && makepkg --noconfirm --syncdeps --install)
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
-tar zxvf yaourt.tar.gz
-(cdyaourt && makepkg --noconfirm --syncdeps --install)
+su - pig --command '
+	wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz &&
+	tar zxvf package-query.tar.gz &&
+	cd package-query &&
+	makepkg --noconfirm --syncdeps --install'
+su - pig --command '
+	wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz &&
+	tar zxvf yaourt.tar.gz &&
+	cd yaourt &&
+	makepkg --noconfirm --syncdeps --install'
