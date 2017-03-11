c417db85a6ebeaf106d5644207bf9c20613ceaec
piglovesyou <thepiglovesyou@gmail.com>
2017-03-11 17:06:25 +0900

    fix yaourt installing failure

--- settings/setup.sh
+++ settings/setup.sh
@@ -18,24 +18,13 @@ __EOF__
 sed -i.bak '/%wheel ALL=(ALL) NOPASSWD: ALL/d' /etc/sudoers
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
-# Packages
-pacman -Syu --noconfirm
-pacman -S \
-    python python-pip \
-    wget vim base-devel \
-    --noconfirm
-
-cat <<EOF >> /etc/pacman.conf
-[archlinuxfr]
-SigLevel = Never
-Server = http://repo.archlinux.fr/arm
-EOF
-
 # yaourt
-cd /tmp
-rm -rf package-query*
-sudo -u pig curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
-sudo -u pig tar zxvf package-query.tar.gz
-cd package-query
-yes | sudo -u pig makepkg -si
-pacman -S yaourt --noconfirm
+su pig
+cd ~
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
+tar zxvf package-query.tar.gz
+(cd package-query && makepkg --noconfirm --syncdeps --install)
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+tar zxvf yaourt.tar.gz
+(cdyaourt && makepkg --noconfirm --syncdeps --install)
+
